086bf3699ebe1682270b5e03cf07ec1bb520ed15
ericj376 <ericellingson376@gmail.com>
2016-02-15 12:00:33 -0700

    created bears

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
 
+var monte = {name: 'monte', id: 0, age: 39, gender: 'male'};
+var paul = {name: 'paul', id: 1, age: 22, gender: 'male'};
+var emilio = {name: 'emilio', id: 2, age: 14, gender: 'male'};
+var escobar = {name: 'escobar', id: 3, age: 21, gender: 'male'};
+
+var bears = [monte, paul, emilio, escobar];
+
 app.get('/api/bears', function(req, res){
-	res.json({title: 'does it work?', body: 'I hope so..'})
+	res.json({bears})
 });
 
 
